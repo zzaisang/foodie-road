c27a60e626c8868fb5233f213956a166d8ccfab4
kimsangjae <zzai_sang@naver.com>
2022-04-25 23:53:58 +0900

    init

---
+++ src/main/java/com/foodieRoad/FoodieRoadApplication.java
@@ -0,0 +1,13 @@
+package com.foodieRoad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieRoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodieRoadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodieRoad/FoodieRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodieRoad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodieRoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
