b37288953850c73d09d701cc1d8ace84b66571d6
kimsangjae <zzai_sang@naver.com>
2022-04-26 01:01:31 +0900

    webflux setting

---
+++ src/main/java/com/foodieRoad/config/WebFluxConfig.java
@@ -0,0 +1,17 @@
+package com.foodieRoad.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+/**
+ * @author zzai_sang
+ * @version 0.1.0
+ * @since 2022/04/26
+ */
+@Configuration
+@EnableWebFlux
+public class WebFluxConfig implements WebFluxConfigurer {
+
+
+}
