0f33cfebc557471d84a5e6fe640e3aedf47792d1
kimsangjae <zzai_sang@naver.com>
2022-05-01 23:56:56 +0900

    domain add

---
+++ src/main/java/com/foodieRoad/domain/Store.java
@@ -0,0 +1,7 @@
+package com.foodieRoad.domain;/**
+ * @author zzai_sang
+ * @version 0.1.0
+ * @since 2022/04/30
+ */
+public class Store {
+}
---
+++ src/main/java/com/foodieRoad/handler/HelloHandler.java
@@ -0,0 +1,7 @@
+package com.foodieRoad.handler;/**
+ * @author zzai_sang
+ * @version 0.1.0
+ * @since 2022/04/27
+ */
+public class HelloHandler {
+}
