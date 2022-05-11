9577a3409290aa65a362764aaf3a9136102d8192
kimsangjae <zzai_sang@naver.com>
2022-05-11 23:57:46 +0900

    kotlin 으로 convert

--- src/main/java/com/foodieRoad/domain/payload/CommonApiParam.kt
+++ src/main/java/com/foodieRoad/domain/payload/CommonApiParam.kt
@@ -1,19 +1,15 @@
-package com.foodieRoad.domain.payload;
+package com.foodieRoad.domain.payload
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.AllArgsConstructor
+import lombok.Getter
 
 /**
  * @author zzai_sang
  * @version 0.1.0
  * @since 2022/05/08
  */
-@Getter
-@AllArgsConstructor
-public enum CommonApiParam {
-
-    START_PAGE("start_page"),
-    END_PAGE("end_page");
-
-    private String value;
+enum class CommonApiParam(
+    val typeName : String
+) {
+    START_PAGE("start_page"), END_PAGE("end_page");
 }
